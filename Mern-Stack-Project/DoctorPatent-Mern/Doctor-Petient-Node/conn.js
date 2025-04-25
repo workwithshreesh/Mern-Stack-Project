d359bba0846128db9a2faab5ecaefb3cfcece2ec
workwithshreesh <shreesht366@gmail.com>
2025-04-25 19:10:31 +0530

    structured

--- Mern-Stack-Project/DoctorPatent-Mern/Doctor-Petient-Node/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectMongo(url){
-    return mongoose.connect(url);
-}
-
-module.exports = {
-    connectMongo
-}
--- Mern-Stack-Project/DoctorPatent-Mern/Doctor-Petient-Node/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {RegisterUsers, LoginUser} = require("../controller/user")
-const router = express.Router();
-
-router.post("/register",RegisterUsers);
-router.post("/login",LoginUser);
-
-module.exports = router;
